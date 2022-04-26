6adef829232314859aa19f284ce02152b96e79c5
LightDDark <77627113+LightDDark@users.noreply.github.com>
2022-04-26 18:35:20 +0300

    cleaned code

--- src/chatPage/MainChat.js
+++ src/chatPage/MainChat.js
@@ -1,13 +1,12 @@
-import User from "../dataBase/User";
 import SubChat from "./SubChat";
 
-function mainChat(){
-    return(
-        <div>
-            <label>
-                <SubChat/>
-            </label>
-        </div>
-    );
+function mainChat() {
+  return (
+    <div>
+      <label>
+        <SubChat />
+      </label>
+    </div>
+  );
 }
 export default mainChat;
