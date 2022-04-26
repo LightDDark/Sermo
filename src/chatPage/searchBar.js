1d9aceae20c22cbf36e506e00561bbaa4f7a26df
LightDDark <77627113+LightDDark@users.noreply.github.com>
2022-04-26 16:38:58 +0300

    added side bar utility

---
+++ src/chatPage/searchBar.js
@@ -0,0 +1,21 @@
+import { useRef } from "react";
+function SearchBar({ doSearch }) {
+  const searchInput = useRef(null);
+  const search = function () {
+    doSearch(searchInput.current.value);
+  };
+
+  return (
+    <div className="searchRow">
+      <input
+        ref={searchInput}
+        onKeyUp={search}
+        type="text"
+        className="control"
+        placeholder="search"
+      ></input>
+    </div>
+  );
+}
+
+export default SearchBar;
